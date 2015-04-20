fc82e061c086bbb3bacc8fdfb540998735df39ce
anger <antgers@gmail.com>
2015-04-21 00:08:13 +0300

    test trial relative importance - no complete

--- test.src.code/test.code.R
+++ test.src.code/test.code.R
@@ -1,6 +1,7 @@
 source("helpers.R")
 library("lubridate")
 library("e1071")
+library("caret")
 
 Dataset <- read.delim("TSIPOYRA-2014 BATCHES-ANON-2.csv", header = TRUE, sep = ";", dec=".")
 data <- create_dataset(Dataset)
@@ -10,31 +11,56 @@ preds <- c("Site", "Region", "Hatchery", "Days", "Econ.FCR.Period", "Actual.Feed
 
 fmla.cl<- as.formula(paste(targ, paste(preds, collapse="+"), sep=" ~ ") ) 
 
-perc = 80/100
-nr <- nrow(data)
-ids <- sort(ceiling(sample( seq(1,nr), nr*perc, replace = FALSE)))
-ds.tr1 <- data[ ids, names(data) %in% unlist(list(preds,targ))]
-ds.ts1 <- data[ -ids, names(data) %in% unlist(list(preds,targ))]
+# perc = 80/100
+# nr <- nrow(data)
+# ids <- sort(ceiling(sample( seq(1,nr), nr*perc, replace = FALSE)))
+# ds.tr1 <- data[ ids, names(data) %in% unlist(list(preds,targ))]
+# ds.ts1 <- data[ -ids, names(data) %in% unlist(list(preds,targ))]
+# 
+# ds.tr1 <- data[ , names(data) %in% unlist(list(preds,targ))]
+# 
+# svm.fit.cl <- tune(svm, fmla.cl, data=ds.tr1, type="nu-classification", kernel="radial", 
+#                     ranges=list(cost=c(0.1,1,10,50,100,150,200), gamma=c(0.5,1,2,3,4)),
+#                                 nu=0.5, 
+#                     tunecontrol = tune.control(sampling ="fix",fix=perc, 
+#                     sampling.aggregate = mean, sampling.dispersion = sd) )  
+#                    
+# best.svm.fit.cl <- svm.fit.cl$best.model
+# pred <- predict(best.svm.fit.cl, ds.ts1 )
+# confmat <- table(true=ds.ts1[,targ],pred)
+# print(confmat)
+# 
+# #print(summary(svm.fit.cl))
+# 
+# res<-list( "Support Vectors per class: "=best.svm.fit.cl$nSV, "Gamma: "= best.svm.fit.cl$gamma,"Cost: "=best.svm.fit.cl$cost,
+#       "Method:"=best.svm.fit.cl$call$method, "Type:"=best.svm.fit.cl$call$type, "Kernel:"=best.svm.fit.cl$call$kernel  )
+# 
+# print(res)
 
+#------------------------------------------------------
 ds.tr1 <- data[ , names(data) %in% unlist(list(preds,targ))]
+ds.tr1.Dummy <- dummyVars( " ~ Site + Region + Hatchery + Days + Econ.FCR.Period + Actual.Feed+ End.Av.Weight + Origin.Month", data=ds.tr1, fullRank=F)
+train.ds <- data.frame(predict(ds.tr1.Dummy, newdata = ds.tr1))
 
-svm.fit.cl <- tune(svm, fmla.cl, data=ds.tr1, type="nu-classification", kernel="radial", 
-                    ranges=list(cost=c(0.1,1,10,50,100,150,200), gamma=c(0.5,1,2,3,4)),
-                                nu=0.5, 
-                    tunecontrol = tune.control(sampling ="fix",fix=perc, 
-                    sampling.aggregate = mean, sampling.dispersion = sd) )  
-                   
-best.svm.fit.cl <- svm.fit.cl$best.model
-pred <- predict(best.svm.fit.cl, ds.ts1 )
-confmat <- table(true=ds.ts1[,targ],pred)
-print(confmat)
+train.dset <- data.frame(train.ds, "Class"=ds.tr1$Class)
 
-#print(summary(svm.fit.cl))
 
-res<-list( "Support Vectors per class: "=best.svm.fit.cl$nSV, "Gamma: "= best.svm.fit.cl$gamma,"Cost: "=best.svm.fit.cl$cost,
-      "Method:"=best.svm.fit.cl$call$method, "Type:"=best.svm.fit.cl$call$type, "Kernel:"=best.svm.fit.cl$call$kernel  )
+fitControl <- trainControl(## 10-fold CV
+  method = "repeatedcv",
+  number = 10,
+  ## repeated ten times
+  repeats = 10)
 
-print(res)
+  
+svmFit <- train(x=ds.tr1.Dummy$vars, y="Class", data=train.dset, 
+                  method = "svmRadial",
+                  trControl = fitControl)
+  
+  
+RocImp <- varImp(  svmFit, scale = FALSE)
+RocImp
+
+plot(RocImp, scale=F)
 
 
 #--------------- cross validation
